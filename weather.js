8b830f5b35d30a7e17af9395ca785dd11c5bd103
Danel Rico <danelricofer@gmail.com>
2024-10-31 10:07:25 +0100

    Prueba

--- weather.js
+++ weather.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 
     $('#buscar').on('click', function(event){
-        var ciudad = $('#ciudad').val();
         var apiKey = '44ee61d1bf2987fb02d4094845e0bbb6';
         var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${ciudad}&limit=5&appid=${apiKey}`
 
